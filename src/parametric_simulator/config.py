eb2ef2ca42586e5db69376397555bfd2d1185926
Eelco van Vliet <eelcovv@gmail.com>
2024-11-08 17:13:37 +0100

    finish later

--- src/parametric_simulator/config.py
+++ src/parametric_simulator/config.py
@@ -1,19 +1,30 @@
+"""
+Type definitions of the configuration file
+"""
+
 from dataclasses import dataclass
-
-from hydra.core.config_store import ConfigStore
-from omegaconf import MISSING
-
-from parametric_simulator.conf.config import Config
 
 
 @dataclass
 class General:
-    max_simulations: int = MISSING
-    script_name: str = MISSING
-    default_args: dict = MISSING
-    path: dict = MISSING
+    max_workers: int
+    script_name: str
+    default_args: dict
+    path: dict
 
 
-cs = ConfigStore.instance()
-cs.store(name="config", node=Config)
-cs.store(name="general", node=General)
+@dataclass
+class Paths:
+    paths: dict
+
+
+@dataclass
+class Rules:
+    rules: dict
+
+
+@dataclass
+class ParametricSimulatorConfig:
+    general: General
+    paths: Paths
+    rules: Rules
--- src/parametric_simulator/parsim.py
+++ src/parametric_simulator/parsim.py
@@ -9,8 +9,10 @@ from pathlib import Path
 
 import hydra
 import yaml
+from hydra.core.config_store import ConfigStore
 
 from parametric_simulator import __version__
+from parametric_simulator.config import ParametricSimulatorConfig
 
 __author__ = "Eelco van Vliet"
 __copyright__ = "Eelco van Vliet"
@@ -82,19 +84,14 @@ def setup_logging(loglevel):
     )
 
 
+cs = ConfigStore.instance()
+cs.store(name="parametric_simulator_config", node=ParametricSimulatorConfig)
+
+
 @hydra.main(version_base=None, config_path=CONFDIR.as_posix(), config_name="config")
-def main(cfg):
-    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
-
-    Instead of returning the value from :func:`fib`, it prints the result to the
-    ``stdout`` in a nicely formatted message.
-
-    Args:
-      cfg:
-      args (List[str]): command line parameters as a list of strings
-          (for example, ``["--verbose", "42"]``).
-    """
+def main(cfg: ParametricSimulatorConfig):
     print(cfg)
+    print(cfg.general.max_workers)
     return
     args = parse_args()
     setup_logging(args.loglevel)
@@ -116,22 +113,8 @@ def main(cfg):
 
 
 def run():
-    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
-
-    This function can be used as an entry point to create console scripts with setuptools.
-    """
     main()
 
 
 if __name__ == "__main__":
-    # ^  This is a guard statement that will prevent the following code from
-    #    being executed in the case someone imports this file instead of
-    #    executing it as a script.
-    #    https://docs.python.org/3/library/__main__.html
-
-    # After installing your project with pip, users can also run your Python
-    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
-    #
-    #     python -m parametric_simulator.skeleton 42
-    #
     run()
